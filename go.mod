9472e1b4fa0e42113b423068bcc87c3d5d73f8a0
uzears <150370163+uzears@users.noreply.github.com>
2025-06-19 22:00:04 +0530

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/uzears/url-shortener
+
+go 1.23.4
+
+require github.com/gorilla/mux v1.8.1
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/uzears/url-shortener/handlers"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/shorten", handlers.ShortenURL).Methods("POST")
+	r.HandleFunc("/{shortID}", handlers.RedirectURL).Methods("GET")
+
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
